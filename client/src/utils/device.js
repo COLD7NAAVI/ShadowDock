8e2dedda6f45f35b9a0a4dd123d123a1f8e1e5f8
COLD7NAAVI <navinheshi@gmail.com>
2026-08-25 12:29:53 +0000

    Fix chat message flow and authentication updates

--- client/src/utils/device.js
+++ client/src/utils/device.js
@@ -20,9 +20,27 @@
 const STORAGE_KEY = "shadowdock_device_identifier";
 
 function generateIdentifier() {
-    return crypto.randomUUID();
-}
 
+    if (
+        typeof crypto !== "undefined" &&
+        typeof crypto.randomUUID === "function"
+    ) {
+        return crypto.randomUUID();
+    }
+
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+
+            const r = Math.random() * 16 | 0;
+            const v = c === "x"
+                ? r
+                : (r & 0x3 | 0x8);
+
+            return v.toString(16);
+        }
+    );
+}
 function getDeviceIdentifier() {
 
     let id = localStorage.getItem(STORAGE_KEY);
